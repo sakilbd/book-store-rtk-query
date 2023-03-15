65ee3eaeabdfbe2a71e1684f4525b29235df91e0
sakilbd <mdsakiluzzaman@gmail.com>
2023-03-15 17:51:24 +0600

    all done

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,37 +1,38 @@
 import React, { useState } from "react";
 import Book from "./Book";
 import { useGetBooksQuery } from "../../features/api/apiSlice";
-import {Error} from '../ui/Error';
-
-
-
+import { Error } from "../ui/Error";
+import { useSelector } from "react-redux";
 
 function Books() {
-  const {data:books,isLoading,isError}= useGetBooksQuery();
-  const [featuredClicked,setFeaturedClicked]=useState(false);
-  
+  const { data: books, isLoading, isError } = useGetBooksQuery();
+  const [featuredClicked, setFeaturedClicked] = useState(false);
+  const { search_text } = useSelector((state) => state.search);
 
   let content = null;
 
-    if (isLoading) {
-        content = <div>Loading</div>
-    }
+  if (isLoading) {
+    content = <div>Loading</div>;
+  }
 
-    if (!isLoading && isError) {
-        content = <div>There are some errors</div>;
-    }
+  if (!isLoading && isError) {
+    content = <div>There are some errors</div>;
+  }
 
-    if (!isLoading && !isError && books?.length === 0) {
-        content = <div>No Books Found</div>
-    }
+  if (!isLoading && !isError && books?.length === 0) {
+    content = <div>No Books Found</div>;
+  }
 
-    if (!isLoading && !isError && books?.length > 0) {
-      let filteredBooks = [...books]
-      if(featuredClicked){
-        filteredBooks = books.filter(book=>book.featured)
-      }
-        content = filteredBooks.map((book) => <Book key={book.id} book={book} />);
+  if (!isLoading && !isError && books?.length > 0) {
+    let filteredBooks = [...books];
+    if (featuredClicked) {
+      filteredBooks = books.filter((book) => book.featured);
     }
+    filteredBooks = filteredBooks.filter((book) =>
+      book?.name?.toLowerCase().includes(search_text)
+    );
+    content = filteredBooks.map((book) => <Book key={book.id} book={book} />);
+  }
   return (
     <div>
       <main class="py-12 px-6 2xl:px-6 container">
@@ -40,13 +41,23 @@ function Books() {
             <h4 class="mt-2 text-xl font-bold">Book List</h4>
 
             <div class="flex items-center space-x-4">
-              <button class={`lws-filter-btn ${!featuredClicked && 'active-filter'}`} onClick={()=>setFeaturedClicked(false)}>All</button>
-              <button class={`lws-filter-btn ${featuredClicked && 'active-filter'}`} onClick={()=>setFeaturedClicked(true)}>Featured</button>
+              <button
+                class={`lws-filter-btn ${!featuredClicked && "active-filter"}`}
+                onClick={() => setFeaturedClicked(false)}
+              >
+                All
+              </button>
+              <button
+                class={`lws-filter-btn ${featuredClicked && "active-filter"}`}
+                onClick={() => setFeaturedClicked(true)}
+              >
+                Featured
+              </button>
             </div>
           </div>
           <div class="space-y-6 md:space-y-0 md:grid grid-cols-1 lg:grid-cols-3 gap-6">
             {/* <!-- Card 1 --> */}
-           {content}
+            {content}
           </div>
         </div>
       </main>
